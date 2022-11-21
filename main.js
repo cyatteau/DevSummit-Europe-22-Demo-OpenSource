b0e15c0a83a0bf1191fd5830cde6f41f177f4891
Courtney Yatteau <112517097+cyatteau@users.noreply.github.com>
2022-11-21 12:54:39 -0500

    Updates to File Structure

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 export const mapContainer = document.getElementById("map-container");
 const theBody = document.getElementById("theBody");
 const form = document.createElement("form");
-let li, query;
+let query;
 form.classList.add("search-form");
 form.setAttribute("autocomplete", "off");
 theBody.appendChild(form);
@@ -9,8 +9,7 @@ export let lat = 51.5072;
 export let long = -0.1186;
 export let view = 14;
 let searchInput = "";
-//create special icon
-import { showPlaces } from "./main3";
+import { showPlaces } from "./main2";
 //create Leaflet map container
 export const map = L.map("map-container", {
   minZoom: 14,
@@ -18,77 +17,76 @@ export const map = L.map("map-container", {
 export const clickedLayerGroup = L.layerGroup().addTo(map);
 export const currentMarkers = [];
 export let clickedData = {};
-import { theSearchInput } from "./main2";
+import { theSearchInput } from "./main3";
 
 // OSM basemap layer
-// L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-//   maxZoom: 14,
-//   attribution:
-//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-// }).addTo(map);
-
-// // CUSTOM basemap layer
-const apiKey = "YOUR_API_KEY";
-import { vectorBasemapLayer } from "esri-leaflet-vector";
-vectorBasemapLayer("e16f851bdec647edba0498e186a5329c", {
-  apiKey: apiKey,
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-// SEARCH FIELD
-// function handleSearch() {
-//   searchInput = document.createElement("input");
-//   searchInput.setAttribute("type", "text");
-//   searchInput.setAttribute("placeholder", "Enter address, city or zip code");
-//   searchInput.setAttribute("id", "search");
-//   form.appendChild(searchInput);
-//   let button = document.createElement("button");
-//   button.setAttribute("id", "search-button");
-//   button.classList.add("btn", "btn-primary");
-//   button.innerHTML = "Search";
-//   form.appendChild(button);
-//   document.querySelector("form").addEventListener("submit", (event) => {
-//     event.preventDefault();
-//     const input = searchInput.value;
-//     queryResults(input);
-//   });
-//   document
-//     .getElementById("search-button")
-//     .addEventListener("click", (event) => {
-//       event.preventDefault();
-//       const input = searchInput.value;
-//       queryResults(input);
-//     });
-// }
-// handleSearch();
+// const apiKey = "YOUR_API_KEY";
+// import { vectorBasemapLayer } from "esri-leaflet-vector";
+// vectorBasemapLayer("e16f851bdec647edba0498e186a5329c", {
+//   apiKey: apiKey,
+// }).addTo(map);
 
 //Nominatim Results
-// query = "[51.5072, -0.1186]";
-// queryResults(query);
-// function queryResults(query) {
-//   fetch(
-//     `https://nominatim.openstreetmap.org/search?format=json&q=post+office+in+${query}`
-//   )
-//     .then((res) => res.json())
-//     .then((jResult) => {
-//       lat = jResult[0].lat;
-//       long = jResult[0].lon;
-//       showThePlaces(jResult);
-//     });
-// }
-// function showThePlaces(jResult) {
-//   for (const marker of currentMarkers) {
-//     map.removeLayer(marker);
-//   }
-//   map.setView([lat, long], view);
-//   for (const result of jResult) {
-//     const position = new L.LatLng(result.lat, result.lon);
-//     currentMarkers.push(
-//       new L.marker(position).addTo(map).bindTooltip(() => {
-//         return L.Util.template(`<b>Name: </b>${result.display_name}<br/>`);
-//       })
-//     );
-//   }
-// }
+query = "[51.5072, -0.1186]";
+queryResults(query);
+function queryResults(query) {
+  fetch(
+    `https://nominatim.openstreetmap.org/search?format=json&q=post+office+in+${query}`
+  )
+    .then((res) => res.json())
+    .then((jResult) => {
+      lat = jResult[0].lat;
+      long = jResult[0].lon;
+      showThePlaces(jResult);
+    });
+}
+
+// SEARCH FIELD
+function handleSearch() {
+  searchInput = document.createElement("input");
+  searchInput.setAttribute("type", "text");
+  searchInput.setAttribute("placeholder", "Enter address, city or zip code");
+  searchInput.setAttribute("id", "search");
+  form.appendChild(searchInput);
+  let button = document.createElement("button");
+  button.setAttribute("id", "search-button");
+  button.classList.add("btn", "btn-primary");
+  button.innerHTML = "Search";
+  form.appendChild(button);
+  document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = searchInput.value;
+    queryResults(input);
+  });
+  document
+    .getElementById("search-button")
+    .addEventListener("click", (event) => {
+      event.preventDefault();
+      const input = searchInput.value;
+      queryResults(input);
+    });
+}
+handleSearch();
+
+function showThePlaces(jResult) {
+  for (const marker of currentMarkers) {
+    map.removeLayer(marker);
+  }
+  map.setView([lat, long], view);
+  for (const result of jResult) {
+    const position = new L.LatLng(result.lat, result.lon);
+    currentMarkers.push(
+      new L.marker(position).addTo(map).bindTooltip(() => {
+        return L.Util.template(`<b>Name: </b>${result.display_name}<br/>`);
+      })
+    );
+  }
+}
 
 if (searchInput == "") {
   theSearchInput.addTo(map);
--- main3.js
+++ main3.js
@@ -1,25 +1,17 @@
-import { map, currentMarkers } from "./main";
 const apiKey = "YOUR_API_KEY";
-import { geocode } from "esri-leaflet-geocoder";
 
-export function showPlaces() {
-  let position;
-  geocode({
-    apikey: apiKey,
-  })
-    .category("Post Office")
-    .nearby(map.getCenter(), 1)
-    .run(function(err, response) {
-      for (const result of response.results) {
-        position = new L.LatLng(result.latlng.lat, result.latlng.lng);
-        currentMarkers.push(
-          new L.marker(position).addTo(map).bindTooltip(() => {
-            return L.Util.template(
-              `<b>${result.properties.PlaceName}</b><br/>${result.properties.Place_addr}<br/>${result.properties.Phone}<br /> ${result.properties.URL}`
-            );
-          })
-        );
-      }
-    });
-  map.setView(position, 14);
-}
+import {
+  geosearch,
+  arcgisOnlineProvider,
+} from "esri-leaflet-geocoder";
+
+export const theSearchInput = geosearch({
+  position: "topright",
+  placeholder: "Enter an address, city, or zip code",
+  useMapBounds: false,
+  providers: [
+    arcgisOnlineProvider({
+      apikey: apiKey,
+    }),
+  ],
+});
